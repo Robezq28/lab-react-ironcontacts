535a0c47953eccf8ae5e7d0603db6ad4184ce4e6
Roberto D. Ezquerro <roberto.ezquerro@gmail.com>
2022-05-03 17:03:56 +0200

    [MAD PP]Paula Moratalla Roberto Ezquerro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+import { useState } from 'react';
 
 function App() {
+  let firstContacts = contacts.slice(0, 5)
+  console.log(firstContacts)
+  const [contactsList, setContactsList] = useState(firstContacts);
+  const addContact = contactID => {
+    const newContact = contactsList.push(contact => contact.id != contactID)
+    setContactsList(newContact)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={() => addContact()}>Add random contact</button>
+        <button>Sort by popularity</button>
+        <button>SOrt by name</button>
+        <table>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+          {firstContacts.map(contact => {
+            return (
+              <tr>
+                <td key={contact.id}><img width='100px' src={contact.pictureUrl} /> </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar ? <p>🏆 </p> : <p> </p>}</td>
+                <td>{contact.wonEmmy ? <p>🏆 </p> : <p> </p>}</td>
+              </tr>
+            )
+          })}
+        </table>
+      </div>
+    </>
+  )
 }
 
 export default App;
